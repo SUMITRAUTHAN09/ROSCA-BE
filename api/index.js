76f19b498709c86fa551ea8c21ec35211ff9d531
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-11-24 01:42:54 +0530

    updated index.js/api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,21 @@
-import app from "../dist/app.js";
+import app from '../dist/app.js';
 
-export default app;
+// Wrap the app to add CORS headers to every response
+const handler = async (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  // Pass to Express app
+  return app(req, res);
+};
+
+export default handler;
